8c0692163bf1a4d332830f1e5afaadb0dc98cf50
vuralkayracetintas <vuralkayracetintass@gmail.com>
2024-02-24 11:11:25 +0300

    code generation

--- module/widgets/test/widgets_test.dart
+++ module/widgets/test/widgets_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:ui_widgets/ui_widgets.dart';
+import 'package:widgets/widgets.dart';
 
 void main() {
   test('adds one to input values', () {
